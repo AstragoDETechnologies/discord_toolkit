08f0651cfe5c1e737889cfa011ce85ab6a5c02a0
AstragoDE <astragon153@gmail.com>
2022-04-28 23:09:18 +0200

    added Coming soon Text

--- lib/src/home_page/tabs/styled_text_generator_tab.dart
+++ lib/src/home_page/tabs/styled_text_generator_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StyledTextGeneratorTab extends StatelessWidget {
   const StyledTextGeneratorTab({Key? key}) : super(key: key);
@@ -6,9 +7,30 @@ class StyledTextGeneratorTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // START: Coming Soon Snippit
       child: Center(
-        child: Text("Styled Text Generator"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Coming Soon",
+              style: GoogleFonts.amaticSc().copyWith(
+                fontSize: 70,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "implementation might take several Weeks",
+              style: GoogleFonts.amaticSc().copyWith(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
+      // END: Coming Soon Snippit
     );
   }
 }
